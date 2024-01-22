31ab2af3b49524788d61fc59d3cbe53678f0272a
Hulcox <hulcox03@gmail.com>
2024-01-22 15:08:14 +0100

    manage the structure of front end

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { computed, ref } from "vue";
-import Drawer from "./components/Drawer.vue";
-import TeamList from "./components/TeamList.vue";
-import Create from "./components/Create.vue";
-import Update from "./components/Update.vue";
-import Delete from "./components/Delete.vue";
+import Drawer from "./components/container/Drawer.vue";
+import TeamList from "./components/view/TeamList.vue";
+import Create from "./components/view/Create.vue";
+import Update from "./components/view/Update.vue";
+import Delete from "./components/view/Delete.vue";
 
 interface RouteConfig {
   [key: string]: any;

6c17316e6dde4ba08f388df000683eca68b8aa76
geoffreyrig4l <grigal@oui.sncf>
2024-01-23 11:36:04 +0100

    REFACTO : add backend + move initial code to frontend

---
+++ frontend/src/Settings.js
@@ -0,0 +1,7 @@
+const config = {
+  serverUrl: "https://door.casdoor.com",
+  clientId: "014ae4bd048734ca2dea",
+  organizationName: "casbin",
+  appName: "app-casnode",
+  redirectPath: "/callback",
+};

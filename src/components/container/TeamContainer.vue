8b2b71ac52c3801fedda6addc6e184272bf561a1
geoffreyrig4l <grigal@oui.sncf>
2024-01-23 10:07:59 +0100

    css shadow

--- src/components/container/TeamContainer.vue
+++ src/components/container/TeamContainer.vue
@@ -6,19 +6,19 @@ defineProps<{
 }>();
 </script>
 <template>
-  <div class="w-96 h-52 bg-base-300 rounded-box p-4 flex flex-col gap-4">
+  <div class="w-96 h-52 bg-base-300 rounded-box p-4 flex flex-col gap-4 shadow">
     <div class="flex gap-4 items-center">
       <img
         :src="'/images/logos/' + urlLogo"
         alt="My Image"
-        width="65"
+        width="60"
         height="auto"
       />
-      <h1 class="text-xl font-semibold">{{ name }}</h1>
+      <h1 class="text-lg font-semibold">{{ name }}</h1>
     </div>
     <div>
-      <h2 class="text-lg font-semibold">Description :</h2>
-      <p class="text-md">{{ description }}</p>
+      <h2 class="text-sm opacity-25 font-semibold italic">Description</h2>
+      <p>{{ description }}</p>
     </div>
   </div>
 </template>

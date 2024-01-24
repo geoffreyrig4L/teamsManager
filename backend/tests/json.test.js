5cc404ed67f5cd70c58a8d146025b589195a75cf
Gassin <killiangassin78@gmail.com>
2024-01-24 16:22:06 +0100

    testing fix

--- backend/tests/json.test.js
+++ backend/tests/json.test.js
@@ -1,15 +1,9 @@
-const fs = require("fs");
-const request = require("supertest");
-const app = require("../server");
-const axios = require('axios');
-
-
-describe('API Test', () => {
-  it('Test for json file reading', () => {
+test('JSON parsing', () => {
+  
     let json = require("../data/teams.json");
     const expected = JSON.stringify({name: 'Paris Saint Germain',
     description: 'Rêvons plus grand',
     urlLogo: 'psg.svg'});
     expect(JSON.stringify(json.psg)).toBe(expected);
-  })
+    
 })

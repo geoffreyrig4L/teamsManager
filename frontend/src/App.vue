a8c9d4979275b3978874500f7acdce25e8d3d198
geoffreyrig4l <grigal@oui.sncf>
2024-01-26 10:24:32 +0100

    verify token all the time

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,40 @@
 <script setup lang="ts">
 import axios from "axios";
 import Drawer from "./components/Drawer.vue";
-import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
+import { onMounted, onUpdated, ref } from "vue";
+import { useCasdoor } from "casdoor-vue-sdk";
+
+const casdoor = useCasdoor();
+
+function login() {
+  window.location.href = casdoor.getSigninUrl();
+  const urlParams = new URLSearchParams(window.location.search);
+  const authorizationCode = urlParams.get('code');
+  if (authorizationCode) {
+    const code : string = authorizationCode;
+    try {
+      axios.get(`http://localhost:8080/getToken?code=${code}`).then((response : any) => {
+        localStorage.setItem("token", response.data.token);
+      })
+    } catch(e) {
+      console.log(e);
+    }
+  }
+}
+
+onUpdated(() => {
+  const token = localStorage.getItem("token") ?? "";
+    try {
+      axios.get(`http://localhost:8080/verifyToken?token=${token}`).then((response : any) => {
+        if(!response.data.active){
+          login()
+        }
+      })
+    } catch(e) {
+      console.log(e);
+    }
+  })
 
 const route = useRoute();
 const teams = ref([]);
@@ -11,7 +43,6 @@ const fetchData = async () => {
   try {
     const res = await axios.get(`http://localhost:8080/teams`);
     teams.value = Object.values(res.data);
-    console.log(teams.value); // Log the teams after fetching
   } catch (error) {
     console.error("Error fetching teams:", error);
   }
